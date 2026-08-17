4b5e5bb793ebd10c9aa09acc9310eeac52d4547f
jack5566-cloud <l6655jack@gmail.com>
2026-08-17 17:20:10 +0800

    商品功能 dao&service實作 增加依商品銷售狀態查詢功能

--- gigafix-backend/src/main/java/com/gigafix/product/repository/ProductDao.java
+++ gigafix-backend/src/main/java/com/gigafix/product/repository/ProductDao.java
@@ -1,6 +1,7 @@
 package com.gigafix.product.repository;
 
 import com.gigafix.product.constant.ProductCategory;
+import com.gigafix.product.constant.ProductSaleStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -17,13 +18,15 @@ public interface ProductDao extends JpaRepository<Product,Long> {
     //用JPQL實作條件查詢
     @Query("SELECT p FROM Product p WHERE " +
             "(:category IS NULL OR p.category = :category) AND " +
-            "(:search IS NULL OR p.productName LIKE %:search%) AND"+
+            "(:saleStatus IS NULL OR p.saleStatus = :saleStatus)AND " +
+            "(:search IS NULL OR p.productName LIKE %:search%) AND "+
             "(:modelName IS NULL OR p.productName LIKE %:modelName%) AND " +
             "(:color IS NULL OR p.productName LIKE %:color%) AND " +
             "(:storage IS NULL OR p.productName LIKE %:storage%) AND " +
             "(:minPrice IS NULL OR p.price >= :minPrice) AND " + // 💡 新增最低價判斷
             "(:maxPrice IS NULL OR p.price <= :maxPrice)")
     Page<Product> findByConditions(@Param("category") ProductCategory category,
+                                   @Param("saleStatus") ProductSaleStatus saleStatus,
                                    @Param("search") String search,
                                    @Param("modelName") String modelName,
                                    @Param("color") String color,
